fb760f4e8cf6da1337f1d7a2fde6f641ec346c16
zengmudong <498490690@qq.com>
2020-05-03 21:51:39 -0400

    Update backend_utilities.R

--- R/backend_utilities.R
+++ R/backend_utilities.R
@@ -5,31 +5,30 @@
 ################################################################################
 
 
-GetAccessionNo <- function(symbol, year, foreign = FALSE) {
-     
-     ##   This is here to please R CMD check
-     filing.year <- NULL
-     filing.name <- NULL
-     accession.no <- NULL
-     
-     year.char <- as.character(year)
-     
-     reports.df <- AnnualReports(symbol, foreign)
-     reports.df <-
-          mutate(reports.df, filing.year = substr(reports.df$filing.date,1,4) ) %>%
-          filter(filing.year == year.char) %>%
-          filter(filing.name == "10-K" | filing.name == "20-F")
-     
-     accession.no.raw <-
-          select(reports.df, accession.no) %>%
-          as.character()
-     
-     ##   Error message for function
-     if(accession.no.raw == "character(0)") {
-          stop("no filings available for given year")
-     }
-     
-     return(accession.no.raw)
+GetAccessionNo <- function(symbol, year, annual = TRUE, quarter = TRUE) {
+  
+  ##   This is here to please R CMD check
+  filing.year <- NULL
+  filing.name <- NULL
+  accession.no <- NULL
+  
+  year.char <- as.character(year)
+  reports.df <- Reports(symbol, annual, quarter )
+  reports.df <-
+    mutate(reports.df, filing.year = substr(reports.df$filing.date,1,4) ) %>%
+    filter(filing.year == year.char) %>%
+    filter(filing.name == "10-K"|filing.name == "10-Q" | filing.name == "20-F")
+  
+  accession.no.raw <-
+    select(reports.df, accession.no) %>%
+    as.character()
+  
+  ##   Error message for function
+  if(accession.no.raw == "character(0)") {
+    stop("no filings available for given year")
+  }
+  
+  return(accession.no.raw)
 }
 
 ################################################################################
